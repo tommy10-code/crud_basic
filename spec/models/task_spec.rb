affc5be434f0c50d803ec00cec09230192f0d9a7
tommy10-code <mtywp058@gmail.com>
2025-08-23 22:02:30 +0900

    factorybot簡単な作成まで

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe "バリデーションチェック" do
-    it "設定したすべてのバリデーションが機能しているか" do end
+    it "設定したすべてのバリデーションが機能しているか" do
       task = Task.new(title: "今日の天気")
-      expect (task).to be_valid
+        expect(task).to be_valid
+    end
   end
 end
