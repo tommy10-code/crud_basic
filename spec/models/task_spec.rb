6acecee93c04f0b67c4f852caf69f07cdfe2b8ce
tommy10-code <mtywp058@gmail.com>
2025-08-23 21:32:50 +0900

    rspec書いてみる

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションチェック" do
+    it "設定したすべてのバリデーションが機能しているか" do end
+      task = Task.new(title: "今日の天気")
+      expect (task).to be_valid
+  end
 end
