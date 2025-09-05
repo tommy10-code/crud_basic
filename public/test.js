bb468beae2bba095aa30e92ca5c50cea25d941a4
tommy10-code <mtywp058@gmail.com>
2025-09-05 15:20:52 +0900

    関数について

--- public/test.js
+++ public/test.js
@@ -2,12 +2,58 @@
 
 /* コメント */
 
-// console.log( 50 + '20');
+// for (let year = 1; year <= 30; year += 1) {
+//   amount *= 1.05;
+//   // amountが200未満の処理は飛ばす
+//   if (amount < 200){
+//     continue ;
+//   }
 
-const n =  Number(prompt('Any number?')) ; 
+//   if (amount > 300) {
+//     // amountが300を超えたら処理が終わる
+//     break;
+//   }
+//   console.log(`Year ${year}: ${amount}`);
+// }
 
-if ( n >= 80) {
-  console.log("A");
-} else {
-  console.log("B");
+// const score = Number(prompt("score?"));
+// console.log(score > 80 ? "A" : "B");
+
+// {
+//   function add(a, b) {
+//   return a + b;
+// }
+// console.log(add(2, 3)); // 5
+
+// }
+
+// const add = (a, b) => {
+//   return a + b;
+// }
+// console.log(add(2, 3)); // 5
+
+// const calculateTotal = (price, amount, rate) => price * amount * rate ;
+// console.log(calculateTotal(100, 10, 1.1));
+
+// const calculateTotall = (price, amount, rate：1.1) => {
+//  return price * amount * rate ;
+// }
+// console.log(calculateTotall(100, 10,rate));
+
+{
+let showa = () => {
+  console.log("---------");
+  console.log("SALE! 50% OFF!");
+  console.log("---------");
+}
+
+let showb = () => {
+  console.log("BREAKING NEWS!");
+  console.log("Two baby pandas born at our Zoo!");
+}
+
+showa();
+showb();
+showa();
+
 }
