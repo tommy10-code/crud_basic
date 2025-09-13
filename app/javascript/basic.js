dc1f8a7ca552a372af94c5702e6b98863df88144
tommy10-code <mtywp058@gmail.com>
2025-09-13 12:32:46 +0900

    js

--- app/javascript/basic.js
+++ app/javascript/basic.js
@@ -1,19 +1,32 @@
 console.log("Hello, Basic!");
 
 {
-  //最大値を取得
-  const numbers = [1, 2, 3, 4, 5];
-  // max = 0として、それより大きかったら、maxを更新していく
-  let max = 0;
-  numbers.forEach((num) => {
-    if (max < num){
-      max = num
-    };
-  });
-  console.log(max);
+  // //最大値を取得
+  // const numbers = [1, 2, 3, 4, 5];
+  // // max = 0として、それより大きかったら、maxを更新していく
+  // let max = 0;
+  // numbers.forEach((num) => {
+  //   if (max < num){
+  //     max = num
+  //   };
+  // });
+  // console.log(max);
 
-  // Math.maxを使って最大値を取得する
-  //スプレッド構文だと、(1, 2, 3, 4, 5)と数字で渡せる
-  console.log(Math.max(...numbers));
-  console.log(Math.min(...numbers));
+  // // Math.maxを使って最大値を取得する
+  // //スプレッド構文だと、(1, 2, 3, 4, 5)と数字で渡せる
+  // console.log(Math.max(...numbers));
+  // console.log(Math.min(...numbers));
+}
+
+{ //大文字小文字で判別するには？
+  //toLowerCase() 入力したものが全て小文字に
+  //toUpperCase() 入力したものが全て大文字に
+  //.trim() 前後の空白をなくす
+  const string = prompt("名前は？");
+
+  if (string.toLowerCase().trim() === 'taro'){
+    console.log('正解');
+  } else {
+    console.log('不正解');
+  }
 }
