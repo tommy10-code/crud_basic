9f7c3d7f6cb597ef0c272886ed7de89f5a6450d8
tommy10-code <mtywp058@gmail.com>
2025-09-13 11:56:24 +0900

    js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./burger"
+import "./basic"
 
 
--- app/javascript/burger.js
+++ app/javascript/burger.js
@@ -3,9 +3,11 @@
   const main = document.getElementById('main')
   const close = document.getElementById('close')
   
+  if (!open || !main || !close) return;
+
+
   open.addEventListener('click', () => {
     main.classList.toggle('-translate-x-full');
-    
   });
 
   close.addEventListener('click', () => {
