5b81b2f725a0c6051c80211b7dfb8741f34fadf3
tommy10-code <mtywp058@gmail.com>
2025-09-29 21:39:29 +0900

    0929

--- app/javascript/basic.js
+++ app/javascript/basic.js
@@ -1,62 +1,41 @@
-{ 
-  class User {
-    name;
-    _score; //getter / setter と組み合わせるための慣習 公開用のプロパティ名と、内部で保持する変数名を区別したい時に使います。
-    //#score #を使うことで外部からアクセスができなくなる。privateフィールドになる
-
-    constructor(name, score) {
-      this.name = name;
-      this._score = score;
-    }
-
-    getUserString() {
-      return `${this.name}: ${this.score}`;
-    }
-
-    get score() {
-      return this._score;
-    }
-
-    set score(newValue) {
-      if (newValue < 0 || newValue > 100) {
-        return console.log('値が不正です'); //早期リターンにより、if文が当てはまれば処理終了
-      }
-      this._score = newValue; //if文とは違う場合は、今回はインタンスの値がそのまま代入
-    }
-  }
-
-  const user = new User('taro', 70);
-  user.score = 1000;        //これを書いたら、セッターが呼ばれる
-  user.score = 50;          //セッターが呼ばれゲッターに50が代入される
-  console.log(user.score);  // ゲッターが呼ばれてその中身が
-}
-
 {
-  class User {
-  constructor(name) {
-    this._name = name; // 実際の値をここに持つ
+  function showHeader(){
+    console.log('Header');
   }
-
-  get name() {
-    // ← getter：読み取り時に自動で呼ばれる
-    console.log("getter発動");
-    return this._name.toUpperCase();
-  }
-
-  set name(newName) {
-    // ← setter：代入時に自動で呼ばれる
-    if (!newName) {
-      console.log("空の名前は不可");
-      return;
+  
+  async function showUsers(){
+    try { //tryとcatch catchでエラーが出てたと金処理をかける
+    const response = await fetch('https://dotinstall.github.io/setup/fetchapi/invalid-users.json') //URL先のjson形式に誤りがあるため、意図的にエラーとなる
+    const users = await response.json();
+    console.log(users);
+    } catch(err) { //(err)は別のでも大丈夫
+      console.log("エラーが発生");
+      console.log("エラー内容" + err);
     }
-    console.log("setter発動");
-    this._name = newName;
   }
-}
 
-const u = new User("taro");
-console.log(u.name);  // ✅ getterが呼ばれて "TARO" を返す
-u.name = "jiro";      // ✅ setterが呼ばれて _name が "jiro" に更新される
-console.log(u.name);  // ✅ getterが呼ばれて "TARO" を返す
+    //fetchの次に行う処理として、.thenを使う asyncとawaitと一緒の意味 今ではasync,awaitを使うことが多い
+  function showUsers() {
+    fetch('https://dotinstall.github.io/setup/fetchapi/invalid-users.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((users) => {
+      console.log(users);
+    })
+    .catch((err) => {
+      console.log("エラーが発生");
+      console.log("エラー内容" + err);
+    });
+  }
+
+
+  function showFooter(){
+    console.log('Footer');
+  }
+
+  showHeader();
+  showUsers();
+  showFooter();
 
 }
